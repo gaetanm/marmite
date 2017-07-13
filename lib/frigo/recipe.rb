1f38fe17d752136cc3020d175d5fbdbdd6abb62c
Gaëtan Masson <im.gaetanmasson@gmail.com>
2017-07-13 22:37:25 +0200

    Add and retrieve more data

--- lib/frigo/recipe.rb
+++ lib/frigo/recipe.rb
@@ -4,34 +4,55 @@ module Frigo
     P_INGREDIENT = 'aqt='
     P_TYPE = 'dt='
 
-    attr_reader :name, :description, :picture, :link
+    attr_reader :name, :author, :picture, :link,
+                :preparation_time, :cooking_time, :nbr_of_people,
+                :detail, :steps, :ingredients
 
     class << self
       def find(ingredient, type = nil)
         ingredient = P_INGREDIENT + ingredient
         type = P_TYPE + type
         page = HTTParty.get(SEARCH_URL+ingredient+'&'+type)
-        build_recipes(Nokogiri::HTML(page))
+        build_from_links(retrieve_link(Nokogiri::HTML(page)))
       end
 
       private
 
-      def build_recipes(parsed_page)
-        recipes = []
-        parsed_page.css('.recette_classique').each do |recipe|
-          name = recipe.css('.m_titre_resultat a')[0]['title']
-          link = recipe.css('.m_titre_resultat a')[0]['href']
-          recipes << Recipe.new(name, nil, nil, link)
-        end
-        recipes
+      def build_from_links(links)
+        links.map { |link| Recipe.new(retrieve_data(link)) }
+      end
+
+      def retrieve_link(parsed_page)
+        parsed_page.css('.recette_classique').map { |node| node.css('.m_titre_resultat a')[0][:href] }
+      end
+
+      def retrieve_data(link)
+        data = {}
+        page = HTTParty.get(ROOT_URL + link)
+        pp = Nokogiri::HTML(page)
+        data[:name] = pp.css('.main-title').text.strip
+        data[:author] = pp.css('.recipe-author__name').text.strip
+        data[:picture] =  pp.css('#af-diapo-desktop-0_img')[0][:src]
+        data[:link] = link
+        data[:preparation_time] = pp.css('.recipe-infos__timmings__preparation').css('.recipe-infos__timmings__value').text.strip
+        data[:cooking_time] = pp.css('.recipe-infos__timmings__cooking').css('.recipe-infos__timmings__value').text.strip
+        data[:nbr_of_people] = pp.css('.recipe-infos__quantity__value').text
+        data[:steps] = pp.css('.recipe-preparation__list__item').map(&:text)
+        data[:ingredients] = pp.css('.recipe-ingredients__list__item').map(&:text).map(&:strip)
+        data
       end
     end
 
-    def initialize(name, description, picture, link)
-      @name        = name
-      @description = description
-      @picture     = picture
-      @link        = link
+    def initialize(params = {})
+      @name        = params[:name]
+      @author      = params[:author]
+      @picture     = params[:picture]
+      @link        = params[:link]
+      @preparation_time = params[:preparation_time]
+      @cooking_time = params[:cooking_time]
+      @nbr_of_people = params[:nbr_of_people]
+      @steps = params[:steps]
+      @ingredients = params[:ingredients]
     end
   end
 end
