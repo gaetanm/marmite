d63b60da9b7c679d91c45d5ab373f89f7fff388d
Gaëtan Masson <im.gaetanmasson@gmail.com>
2017-07-13 23:11:38 +0200

    Add spec

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -6,5 +6,19 @@ RSpec.describe Frigo::Recipe do
       recipes = Frigo::Recipe.find('fraise', 'dessert')
       expect(recipes.count).to eql(10)
     end
+
+    it 'fills the returned recipes' do
+      recipe = Frigo::Recipe.find('fraire', 'dessert').first
+      expect(recipe).to have_attributes(
+        name: 'Tiramisu aux fraises',
+        author: 'Anonyme',
+        picture: 'https://image.afcdn.com/recipe/20160404/2834_w397h246c1cx1500cy1000.jpg',
+        link: '/recettes/recette_tiramisu-aux-fraises_14039.aspx',
+        preparation_time: '20 min',
+        cooking_time: '',
+        nbr_of_people: '4',
+        steps: include("Préparer 2 h à l'avance."),
+        ingredients: include('250 g de mascarpone'))
+    end
   end
 end
